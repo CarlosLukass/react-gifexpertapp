af8b714c8797e9a48d0c4f9c552a040c82c6641c
Carlos Lucas <carlos.lukassf@gmail.com>
2020-08-21 23:45:09 -0500

    first commit

---
+++ docs/precache-manifest.7eda398a5c93891ef77299cde63cdb65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d29d0321ecc5dcd1f3ba7c62ed9893",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb66875f5e8e1a007ef5",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "83519745faadb462c993",
+    "url": "/static/css/main.d5d9d53e.chunk.css"
+  },
+  {
+    "revision": "bb66875f5e8e1a007ef5",
+    "url": "/static/js/2.c1033e28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c1033e28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83519745faadb462c993",
+    "url": "/static/js/main.2c032924.chunk.js"
+  },
+  {
+    "revision": "e13af855f2385358fc05",
+    "url": "/static/js/runtime-main.17c06fbb.js"
+  }
+]);
